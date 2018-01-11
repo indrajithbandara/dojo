a57057378acdf7e952751281d0d6c3b331503c16
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2018-01-11 23:25:31 +0900

    Added sample node for CI

---
+++ nodejs/sample_node_for_ci/index.js
@@ -0,0 +1,6 @@
+/** 与えられた値を2 で割って小数点以下を切り捨てた結果を返す関数 */
+exports.calculate = function(num) {
+    return 0;    // FIXME: 要件を満たすソースコードを書く
+    // return Math.floor(num / 2);
+    //
+};
---
+++ nodejs/sample_node_for_ci/test/test-index.js
@@ -0,0 +1,15 @@
+var chai = require('chai'),
+    should = chai.should();
+var divided = require('../index.js');
+
+describe('divided', function() {
+    describe('#calculate', function() {
+        it('should return 2 when the value is 4', function() {
+            divided.calculate(4).should.equal(2);
+        });
+        it('should return 1 when the value is 3', function() {
+            divided.calculate(3).should.equal(1);
+        });
+    });
+});
+
